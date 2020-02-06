b6ea1d5e085fe87e44a4e92c12ca81106d106f10
Anshuman <anshuman.web@gmail.com>
2020-02-06 05:47:10 +0530

    Updates

---
+++ precache-manifest.af160db1cab6750b672a190d59e06295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c647a56a3b5a31ec660ea45faef99f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f20319be54671edf9a4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "be6312d94713c05804c3",
+    "url": "/static/js/2.fb1e7d87.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fb1e7d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f20319be54671edf9a4",
+    "url": "/static/js/main.ec4284f6.chunk.js"
+  },
+  {
+    "revision": "963d3ff202380122c9b6",
+    "url": "/static/js/runtime-main.9859aeaf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
